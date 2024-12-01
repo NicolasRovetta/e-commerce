8d1e507f1100168a87779f8fab7f2a1b1cb58548
NicolasRovetta <nico.rovetta@gmail.com>
2024-12-01 20:03:33 -0300

    saque contador

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,24 +3,12 @@ import cart from '../assets/cartWidget.png'
 import { useState } from 'react'
 
 function CartWidget() {
-  const [cartItems, setCartItems] = useState(0);
-
-  const handleAddItem = () => {
-    setCartItems(cartItems + 1);
-  };
-
-  const handleRemoveItem = () => {
-    if (cartItems > 0) {
-      setCartItems(cartItems - 1);
-    }
-  };
-
   return (
     <>
-      <img src={cart} alt="logoCart" className='logoCart' onClick={handleAddItem} />
-      <div onClick={handleRemoveItem}>Carrito ({cartItems})</div>
+    <img src={cart} alt="logoCart" className='logoCart'/>
+    <div>carrito</div>
     </>
-  );
+  )
 }
 
-export default CartWidget;
+export default CartWidget
