e81069d34cf87b1181f909af9e0f31fa6d315da1
NicolasRovetta <nico.rovetta@gmail.com>
2025-01-19 23:05:20 -0300

    creat btn add cart+modifications of css

--- src/components/DetalleProducto.jsx
+++ src/components/DetalleProducto.jsx
@@ -1,50 +1,65 @@
-import { useParams } from 'react-router-dom'
-import Error from './Error'
-import { getFirestore, doc, getDoc } from 'firebase/firestore';
-import { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import Error from "./Error";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { useContext } from 'react'
+import ThemeContext from './themeContext'
+
+export const sendCart = (producto) => {
+  console.log('Producto a agregar:', producto); // Agregar este log para depuración
+  const cart = JSON.parse(localStorage.getItem('cart')) || []; // Obtener el carrito actual del localStorage    
+  cart.push(producto);  // Agregar el producto actual al carrito    
+  localStorage.setItem('cart', JSON.stringify(cart));// Guardar el carrito actualizado en el localStorage
+  alert('Producto agregado al carrito');
+};
 
 function DetalleProducto() {
-    const { id } = useParams();
-    const [producto, setProducto] = useState(null);
-    const [loading, setLoading] = useState(true); // Estado para manejar la carga
+  const {theme} = useContext(ThemeContext)
+  const { id } = useParams();
+  const [producto, setProducto] = useState(null); // Definición del estado producto
+  const [loading, setLoading] = useState(true); // Estado para manejar la carga
 
-    useEffect(() => {
-        const fetchProducto = async () => {
-            const db = getFirestore();
-            const productoDoc = doc(db, 'productos', id);
-            const productoSnapshot = await getDoc(productoDoc);
-            if (productoSnapshot.exists()) {
-                setProducto({ id: productoSnapshot.id, ...productoSnapshot.data() });
-            } else {
-                setProducto(null);
-            }
-            setLoading(false); 
-        };
+  useEffect(() => {
+    const fetchProducto = async () => {
+      const db = getFirestore();
+      const productoDoc = doc(db, "productos", id);
+      const productoSnapshot = await getDoc(productoDoc);
+      if (productoSnapshot.exists()) {
+        setProducto({ id: productoSnapshot.id, ...productoSnapshot.data() }); // Actualización del estado producto
+      } else {
+        setProducto(null);
+      }
+      setLoading(false);
+    };
 
-        fetchProducto();
-    }, [id]);
+    fetchProducto();
+  }, [id]);
 
-    if (loading) {
-        return <div>Cargando...</div>; 
-    }
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
 
-    if (!producto) {
-        return (
-            <div className='errorFullScreen'>
-                <Error />
-            </div>
-        );
-    }
-
+  if (!producto) {
     return (
-        <div className='detalleProducto'>
-            <h2>{producto.modelo}</h2>
-            <p>{producto.descripcion}</p>
-            <h3><strong>Precio: ${producto.precio}</strong></h3>
-            {/*<img src={producto.imagen} alt={producto.modelo} />*/}            
-            <button>Agregar al carrito</button>            
-        </div>
+      <div className="errorFullScreen">
+        <Error />
+      </div>
     );
+  }
+
+  return (
+    <div className={`backgroundDetalleProducto app ${theme}`}>
+      <div className="detalleProducto">
+        <h2>{producto.modelo}</h2>
+        <p>{producto.descripcion}</p>
+        <h3>
+          <strong>Precio: ${producto.precio}</strong>
+        </h3>
+        {/*<img src={producto.imagen} alt={producto.modelo} />*/}
+        <button onClick={() => sendCart(producto)}>Agregar al carrito</button>      
+      </div>
+    </div>
+  );
 }
 
 export default DetalleProducto;
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
+import { sendCart } from "./DetalleProducto";
 
-function Item({  categoria, modelo, precio, descripcion, id }) {
+function Item({ categoria, modelo, precio, id }) {
+  const producto = { categoria, modelo, precio, id };
+
   return (
-    <Link to={`/productos/${id}`} className="card">
-      <div>{categoria}</div>
-      <div><h3>{modelo}</h3></div>
-      {/*<img src={producto.imagen} alt={producto.modelo} />*/}      
-      <div><strong>Precio: ${precio}</strong></div>
-      <button >Agregar al carrito</button>
-    </Link>
+    <div className="card">
+      <Link to={`/productos/${id}`}>
+        <div>{categoria}</div>
+        <div>
+          <h3>{modelo}</h3>
+        </div>
+        {/*<img src={producto.imagen} alt={producto.modelo} />*/}
+        <div>
+          <strong>Precio: ${precio}</strong>
+        </div>
+      </Link>
+      <button onClick={() => sendCart(producto)} className="buttonAdd">Agregar al carrito</button>    
+    </div>
   );
 }
 
