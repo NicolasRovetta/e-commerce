e52c61a7dd353f12229415737b895355fced01b7
NicolasRovetta <nico.rovetta@gmail.com>
2025-02-13 22:40:36 -0300

    secret apiKey

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYSAlgzFGUr3pyuzHijKoZRrorOgRVK9M",
+  apiKey: import.meta.env.VITE_apiKey,
   authDomain: "ie-commerce.firebaseapp.com",
   projectId: "ie-commerce",
   storageBucket: "ie-commerce.firebasestorage.app",
